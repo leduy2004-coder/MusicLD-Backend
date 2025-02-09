e89af33dd24159138f668e400584d395fc50207e
leduy2004-coder <leduy8762@gmail.com>
2025-02-09 21:37:06 +0700

    deloy redis in upstash

--- src/main/java/com/myweb/MusicLD/config/RedisConfig.java
+++ src/main/java/com/myweb/MusicLD/config/RedisConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -26,13 +27,23 @@ public class RedisConfig {
     private String redisHost;
     @Value("${spring.data.redis.port}")
     private int redisPort;
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration configuration =
                 new RedisStandaloneConfiguration(redisHost, redisPort);
-        return new LettuceConnectionFactory(configuration);
+
+        configuration.setPassword(redisPassword); // Thêm password nếu có
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .useSsl() // Kích hoạt SSL
+                .build();
+
+        return new LettuceConnectionFactory(configuration, clientConfig);
     }
+
 
     @Bean(name = "myRedisTemplate")
     <K,V>RedisTemplate<K, V> redisTemplate() {
