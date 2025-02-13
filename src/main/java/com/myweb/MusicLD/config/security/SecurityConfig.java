7c29bf71cb30c9028856377dcbec124b92b2b4b0
leduy2004-coder <leduy8762@gmail.com>
2025-02-13 20:21:39 +0700

    fix function follow and data.sql

--- src/main/java/com/myweb/MusicLD/config/security/SecurityConfig.java
+++ src/main/java/com/myweb/MusicLD/config/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.myweb.MusicLD.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
--- src/main/java/com/myweb/MusicLD/repository/jpa/FollowerRepository.java
+++ src/main/java/com/myweb/MusicLD/repository/jpa/FollowerRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigInteger;
 import java.util.List;
@@ -17,6 +18,7 @@ public interface FollowerRepository extends JpaRepository<FollowerEntity, BigInt
     FollowerEntity findBySenderAndReceiver(UserEntity sender, UserEntity receiver);
 
     @Modifying
+    @Transactional
     @Query("UPDATE FollowerEntity f SET f.status = :status WHERE f.sender.id = :idSender AND f.receiver.id = :idReceiver")
     void updateStatus(@Param("idSender") BigInteger idSender, @Param("idReceiver") BigInteger idReceiver, @Param("status") RequestFollowStatus status);
 
